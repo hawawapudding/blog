686f28aef4c429973a1eb17718140bce81515d17
Ushida Yosei <nezowarui0504@gmail.com>
2023-08-16 06:14:49 +0900

    Chapter4.7までのコード

---
+++ components/post-body.tsx
@@ -0,0 +1,4 @@
+import styles from '@/styles/post-body.module.css'
+export default function PostBody({ children }: { children: React.ReactNode }) {
+  return <div className={styles.stack}>{children}</div>
+}
